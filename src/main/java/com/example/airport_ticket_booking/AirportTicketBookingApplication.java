60a515ba4c506cc10231df8e121290b924dfba72
amrbadran <badran.amr2003@gmail.com>
2024-07-31 17:17:02 +0300

    initial commit

---
+++ src/main/java/com/example/airport_ticket_booking/AirportTicketBookingApplication.java
@@ -0,0 +1,13 @@
+package com.example.airport_ticket_booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportTicketBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirportTicketBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/airport_ticket_booking/AirportTicketBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airport_ticket_booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportTicketBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
